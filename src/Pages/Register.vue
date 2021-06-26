534cd9be37f8ef1c9ffb96f3bb2c772bacb82d52
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-06-26 09:51:07 +0530

    ADDED : scss to register page

--- src/Pages/Register.vue
+++ src/Pages/Register.vue
@@ -1,49 +1,43 @@
 <template>
   <div class="main">
-    <form>
+    <form class="" action="Register.vue">
       <div class="head">
-        <h2>
-          <title><h2>Fundoo</h2></title>
-        </h2>
-        <h3>Create your Fundoo Account</h3>
+        <h3>Create your Google Account</h3>
       </div>
       <div class="name">
-        <input type="name" required pattern="[A-Za-z]{3,10}" />
+        <input type="name" name="" value="" required />
         <label>First name</label>
       </div>
       <div class="name">
-        <input type="name" required pattern="[A-Za-z]{3,10}" />
+        <input type="name" required />
         <label>Last name</label>
       </div>
-      <div class="email">
-        <input
-          type="email"
-          value="@gmail.com"
-          pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-          required/>
-        <label>email</label>
+      <div class="user-name">
+        <input type="username" required />
+        <label>Username</label>
+        <span class="gmail">@gmail.com</span>
       </div>
-        <a class="">You can use letters, numbers & periods</a>
-      <div class="password">
-        <input
-          type="password"
-          required
-          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"/>
+      <a class="line1">You can use letters, numbers & periods</a>
+      <a  class="line2">Use my current email address instead</a>
+      <div class="pass">
+        <input type="password" class="password" id="pass" required />
         <label>Password</label>
       </div>
-      <div class="password">
-        <input
-          type="password"
-          required
-          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"/>
+      <div class="pass">
+        <input type="password" class="password" id="pass" required />
         <label>Confirm</label>
       </div>
-      <a class="">Use 8 or more characters with a mix of letters,numbers & symbols></a>
-      <a href="http://localhost:8080/login">Sign in instead</a>
-      <input type="submit" value="Next">
+      <div class="iconeye">
+        <img src="../assets/eyelogo.png" id="eye" />
+      </div>
+      <a class="line3"
+        >Use 8 or more characters with a mix of letters, numbers & symbols</a
+      >
+      <a href="http://localhost:8080/login" class="line4">Sign in instead</a>
+      <input type="submit" value="Next" />
     </form>
-    <div class="side-logo">
-         <img src="../assets/googlesidelogo.jpg" alt="side-image">
+    <div class="side-image">
+      <img src="../assets/googlesidelogo.png" alt="" id="side-logo" />
     </div>
   </div>
 </template>
@@ -56,4 +50,6 @@ export default {
 };
 </script>
 
-
+<style lang="scss">
+@import "../styles/Register.scss";
+</style>
